c7b6d48b3c4cf1038d23607bf7845297625bbb03
mun-kyeong <horse6953@naver.com>
2024-11-29 17:30:06 +0900

    [CHORE] : MUI 설치

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { Button } from "@mui/material";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Button variant="contained">Click me!</Button>
       </header>
     </div>
   );
